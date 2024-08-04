58537bd372153b83fa1d0c734e9a67715386afe4
Bishnutosh <bishnutoshpanda@gmail.com>
2024-08-04 21:24:11 +0530

    About and Experience section completed.

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,8 +121,7 @@ import {
       icon: starbucks,
       iconBg: "#383E56",
       date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+      points: ["Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
